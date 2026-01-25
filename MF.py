d8ea33919263ceda1173d39001333fb9e53cc951
damidirad <85406408+damidirad@users.noreply.github.com>
2026-01-25 11:50:54 +0100

    Update MF.py

--- MF.py
+++ MF.py
@@ -6,8 +6,8 @@ Original author: islam
 
 import torch
 import torch.nn as nn
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from helpers import get_device
+device = get_device()
 
 #%%
 # Collaborative Filtering
